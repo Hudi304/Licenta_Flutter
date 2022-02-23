ac199c08b20df410e11a96ffaf0efb07b77918c9
Hudi304 <hudisteanum@yahoo.com>
2022-02-23 12:23:57 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'home-page/home-page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const title = 'WebSocket Demo';
+    return const MaterialApp(
+      title: title,
+      home: HomePage(),
+    );
+  }
+}
